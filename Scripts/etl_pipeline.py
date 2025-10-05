281e770b066a8350feaa0f7121628ae7b9117285
Anukiran <anukiranbathula04@gmail.com>
2025-10-05 19:52:18 +0530

    Update etl_pipeline.py

--- Scripts/etl_pipeline.py
+++ Scripts/etl_pipeline.py
@@ -1,6 +1,1 @@
-DB_CONFIG = {
-    'host': 'localhost',       # MySQL server host
-    'user': 'root',            #  MySQL username
-    'password': 'yourpassword',# MySQL password
-    'database': 'retail_db'    # database created
-}
+
